350028b38f5fa0e8f01cf5999d601634f6902c2e
Viktor Rura <ruravictor@gmail.com>
2023-10-25 23:54:58 +0200

    Switched debug to true to fix 500 and 400 errors

--- pura_vida/settings.py
+++ pura_vida/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = True
 
-ALLOWED_HOSTS = ['pura-vida-611d56be2c03.herokuapp.com', 'localhost', '']
+ALLOWED_HOSTS = ['pura-vida-611d56be2c03.herokuapp.com', '127.0.0.1', '']
 
 
 # Application definition
