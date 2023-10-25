06290ffa2a764969550fd255c3913c85fb024429
Viktor Rura <ruravictor@gmail.com>
2023-10-25 22:34:09 +0200

    removed secret key and set debug

--- pura_vida/settings.py
+++ pura_vida/settings.py
@@ -22,11 +22,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-v+!1oajy^rm@-w6bu@5q8h\
-    x!_5=5(zur563dkks5-_m4i(r&ls'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['pura-vida-611d56be2c03.herokuapp.com', 'localhost', '']
 
