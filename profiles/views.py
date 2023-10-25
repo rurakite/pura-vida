7e0e9e4da614c79c8148a619d314c7ad3dcc7c7b
Viktor Rura <ruravictor@gmail.com>
2023-10-25 20:58:10 +0200

    Secured the views

--- profiles/views.py
+++ profiles/views.py
@@ -3,10 +3,11 @@ from django.contrib import messages
 
 from .models import UserProfile
 from .forms import UserProfileForm
-
+from django.contrib.auth.decorators import login_required
 from checkout.models import Order
 
 
+@login_required
 def profile(request):
     """ Display the user's profile. """
     profile = get_object_or_404(UserProfile, user=request.user)
